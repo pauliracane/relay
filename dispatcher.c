ff2af7ea4cbebfa4f94a8d9b119fcbc8864d45dc
pauliracane <smallmaddog11@yahoo.com>
2016-05-13 15:44:47 -0400

    Added comments explaining code slightly

--- dispatcher.c
+++ dispatcher.c
@@ -12,31 +12,41 @@
 
 int main(void)
 {	
+	//Initialize character array to take in user input that will later
+	//be passed to listener
 	char str[MAX_SZ] = {'\0'};
-
+	
+	//Create file w/ 777 permissions
 	int filedesc = open("/tmp/testfile.txt", O_WRONLY | O_CREAT | O_TRUNC,
 						S_IRWXU | S_IRWXG | S_IRWXO);
+	//If file failed to be made, tell them it's broken, exit
 	if(filedesc < 0)
 	{
 		    printf("Broken.\n");
 			    return 1;
 	}
+	//Actually give listeners somethign to latch on to
 	write(filedesc, "", 22);
 	close(filedesc);
 	
 	while (1)
 	{
+		//Wait for input; if input is Nothin, or input is Ctrl + D, break
 		if ( (fgets(str, MAX_SZ-1 , stdin) ) == NULL || str[0]==0x04)
 		{
 			break;
 		}
+		//Reopen file descriptor
 		int filedesc = open("/tmp/testfile.txt", O_WRONLY | O_CREAT | O_TRUNC);
+		//If file descriptor is null; it failed
 		if(filedesc < 0)
 		{
 			printf("Broken.\n");
 			return 1;
 		}
+		//Set lock on file
 		flock(filedesc, LOCK_EX);
+		//Overwrite file; if you don't print entire message, break
 		if(write(filedesc,str, strlen(str)) != (int) strlen(str))
 		{
 			write(filedesc,"There was an error writing to testfile.txt\n", 36);
@@ -44,10 +54,12 @@ int main(void)
 			// characters than requested to be written.
 			return 1;
 		}
-
+		//Unlock file
 		flock(filedesc, LOCK_UN);
+		//Close file descriptor
 		close(filedesc);
 	}
+	//Remove file
 	remove("/tmp/testfile.txt");
 	return 0;
 }
--- listener.c
+++ listener.c
@@ -10,13 +10,18 @@
 
 int main()
 {
+	//Declare storage spaces
 	char buff[MAX_SZ-1] = {'\0'};
 	char input[MAX_SZ-1] = {'\0'};
-	for(;;)
+	while (1)
 	{
+		//Open file for Read
 		int filedesc = open("/tmp/testfile.txt", O_RDONLY);
+		//Error check the opening of the file
 		if (filedesc < 0)
 		{
+			//If you've gone through teh loop, you can be sure the 
+			//dispatcher closed
 			if ( input[0] )
 			{
 				printf("Dispatcher has closed.\n");
@@ -28,17 +33,23 @@ int main()
 			close(filedesc);
 			return 1;
 		}
+		//Put shared lock on file
 		flock(filedesc, LOCK_SH);
+		//Wipe Input
 		for (int x = 0; x < MAX_SZ-1; x++)
 			input[x] = '\0';
+		//REad in file
 		if(read(filedesc,input,MAX_SZ))
 		{
+			//Compare to last iteration
+			//If it's changed, print new line
 			if (strcmp(buff, input))
 			{
 				strncpy(buff, input, MAX_SZ-1);
 				printf("%s", buff);
 			}
 		}
+		//Unlock
 		flock(filedesc, LOCK_UN);
 		close(filedesc);
 	}
